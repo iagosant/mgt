9f4f6d58d70044881f8ddf639423336b1c63b3e5
misleyrm <misley.rodriguez@yahoo.com>
2017-05-24 15:07:35 -0400

    My changes

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -18,6 +18,17 @@ $(document).on('turbolinks:load',function(){
     if (pathname == "/"){
       video.play();
     }
+
+    $('span.ms-page').html($('.nav-menu a.active').html());
+
+    $('.nav-menu a').click(function(e){
+
+      $('span.ms-page').slideUp('slow'); // you may want this to be 'fast'
+      $('span.ms-page').html($('.nav-menu a.active').html());
+      $('span.ms-page').slideDown('slow');
+
+    })
+
     $("#video_overlay .text").click(function(){
       var $this = $(this);
       var $container = $(".video");
@@ -34,16 +45,38 @@ $(document).on('turbolinks:load',function(){
     });
 
     /** Do it for window load ***/
-    $(window).resize(function(){
-        // alert($('aside.right').height());
-        var size = $('aside.right').height() - 93;
-        size = size + 'px';
-        if ($('.container').width() <= 1020 ){
-                  $('.content').css('height', size ) ;
-                  $('.content').css('overflow','scroll');
-                  $('.content').css('padding-left','10px');
+    // $(window).resize(function(){
+    //     // alert($('aside.right').height());
+    //     var size = $('aside.ms-col-img figure').height() - 93;
+    //     size = size + 'px';
+    //     if ($('.container').width() <= 1020 ){
+    //               $('.content').css('height', size ) ;
+    //               $('.content').css('overflow','scroll');
+    //               $('.content').css('padding-right','10px');
+    //        }
+    //
+    // });
+  //   $("a[href='#menuExpand']").click(function(e) {
+   //
+  //      $(".nav-menu").toggleClass("menuOpen");
+  //      e.preventDefault();
+  //  });
 
-           }
+   $('.mobile-toggle').click(function() {
+    if ($(".main-header").hasClass('menuOpen')) {
+        $(".main-header").removeClass('menuOpen');
+    } else {
+        $(".main-header").addClass('menuOpen');
+    }
+});
 
+     // Sticky Header
+    $(window).scroll(function() {
+
+        if ($(window).scrollTop() > 100) {
+            $('#header').addClass('sticky');
+        } else {
+            $('#header').removeClass('sticky');
+        }
     });
 });
